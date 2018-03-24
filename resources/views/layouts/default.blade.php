0d8671087138c886dc0bebbf2d64da34a89c175d
Koke_Cacao <i@kokecacao.me>
2018-03-24 15:45:06 +0000

    Fixing bugs. Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Sample') - Laravel</title>
+    <link rel="stylesheet" href="/css/app.css">
+    <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
   </head>
   <body>
-    @yield('content')
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
   </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@
 //     return view('welcome');
 // });
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('abouy', 'StaticPagesController@about');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/help', 'StaticPagesController@help')->name('help');
+Route::get('about', 'StaticPagesController@about')->name('about');
+Route::get('signup', 'UsersController@create')->name('signup');
